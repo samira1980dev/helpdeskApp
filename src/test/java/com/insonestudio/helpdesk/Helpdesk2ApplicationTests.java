49ba966731e3d47a8f77e690abef38066acc4fa3
Samira Marcela Roman <samira1980dev@gmail.com>
2024-05-29 15:57:20 -0300

    Projeto iniciado

---
+++ src/test/java/com/insonestudio/helpdesk/Helpdesk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insonestudio.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Helpdesk2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
